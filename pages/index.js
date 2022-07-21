647fd5e66bf178e9f5b784cf6c8e553b7a33fa32
tsots <mosalatsotetsi@gmail.com>
2022-07-21 13:38:25 +0200

    changed index.js

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <div>Hello World!!From Tsotetsi</div>;
+  return (
+    <div>
+      <h4>Hello World!!From Tsotetsi</h4>
+      <p>Holla Seven</p>
+    </div>
+  );
 }
